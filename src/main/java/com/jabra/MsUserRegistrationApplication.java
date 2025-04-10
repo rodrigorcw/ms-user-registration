3c6282e886ea9ad3a05c66bc10216056a5315263
rodrigo wenzel <rodrigocw90@gmail.com>
2025-04-10 06:22:30 -0300

    Initial Commit

---
+++ src/main/java/com/jabra/MsUserRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.jabra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUserRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUserRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jabra/MsUserRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jabra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUserRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
